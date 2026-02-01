61711ed75ac3cd1d95be8ffb298a076d5280c0dc
Mustafa-3000-xyz <mustafagit234@gmail.com>
2026-02-01 08:42:23 +0200

    Editing structrue

--- src/Components/Trainers-page/Add-trainer/Add_Trainer.tsx
+++ src/Components/Trainers-page/Add-trainer/Add_Trainer.tsx
@@ -6,7 +6,7 @@ import Trainer_Info_Form from "./Trainer-info-form/Trainer_Info_Form";
 import Date_Info from "./Subscription-info/Date-info/Date_Info";
 import Add_Subscription_From_Settings from "./Subscription-info/Add-subscription-from-settings/Add_Subscription_From_Settings";
 import { regexPhone } from "@/REGEX";
-import Subscription_Info_Form from "./Subscription-info/Subscription_Info_Form";
+import Subscription_Info_Form from "./Subscription-info/Subscription-info-form/Subscription_Info_Form";
 import { Flip, toast } from "react-toastify";
 // ========================================================== //
 interface Add_Trainer_Props {
